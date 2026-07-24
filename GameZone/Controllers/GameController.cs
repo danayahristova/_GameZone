a25dbe89e80ba566408885794dc503c104795f47
Danaya <d.hristova@is-bg.net>
2026-07-24 16:58:53 +0300

    Add action method added to the game controller

--- GameZone/Controllers/GameController.cs
+++ GameZone/Controllers/GameController.cs
@@ -1,4 +1,5 @@
 ﻿using GameZone.Data;
+using GameZone.Data.Models;
 using GameZone.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -11,21 +12,52 @@ namespace GameZone.Controllers
         public GameController(GameZoneDbContext context)
         {
             _context = context;
-        }   
+        }
         public IActionResult All()
         {
-            var games =  _context.Games
+            var games = _context.Games
                 .Select(g => new GameViewModel
-                    {
-                        Id = g.Id,
-                        Title = g.Title,
-                        Description = g.Description,
-                        ImageUrl = g.ImageUrl,
-                        Publisher = g.PublisherName,
-                        ReleasedOn = g.ReleasedOn,
-                        GenreId = g.GenreId
-                    }).ToList();
+                {
+                    Id = g.Id,
+                    Title = g.Title,
+                    Description = g.Description,
+                    ImageUrl = g.ImageUrl,
+                    Publisher = g.PublisherName,
+                    ReleasedOn = g.ReleasedOn,
+                    GenreId = g.GenreId
+                }).ToList();
             return View(games);
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            var genres = _context.Genres.ToList();
+            var model = new GameAddViewModel
+            {
+                Genres = genres
+            };
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult Add(GameAddViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                model.Genres = _context.Genres.ToList();
+                return View(model);
+            }
+            var game = new Game
+            {
+                Title = model.Title,
+                Description = model.Description,
+                ImageUrl = model.ImageUrl,
+                PublisherName = model.PublisherName,
+                ReleasedOn = model.ReleasedOn,
+                GenreId = model.GenreId
+            };
+            _context.Games.Add(game);
+            _context.SaveChanges();
+            return RedirectToAction("All");
         }
     }
 }
--- GameZone/ViewModels/GameAddViewModel.cs
+++ GameZone/ViewModels/GameAddViewModel.cs
@@ -4,11 +4,8 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GameZone.ViewModels
 {
-    public class GameViewModel
+    public class GameAddViewModel
     {
-        [Key]
-        public int Id { get; set; }
-
         [Required]
         public string Title { get; set; } = string.Empty;
 
@@ -18,15 +15,13 @@ namespace GameZone.ViewModels
         public string? ImageUrl { get; set; }
 
         [Required]
-        public string Publisher { get; set; } = string.Empty;
+        public string PublisherName { get; set; } = string.Empty;
 
         [Required]
         public DateTime ReleasedOn { get; set; }
 
         [Required]
         public int GenreId { get; set; }
-
-        [ForeignKey(nameof(GenreId))]
-        public Genre Genre{ get; set; } = null!;
+        public List<Genre> Genres { get; set; } = new List<Genre>();
     }
 }
