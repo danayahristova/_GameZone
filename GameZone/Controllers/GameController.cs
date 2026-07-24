366385ee600cb2310d927cc42bd08f173c620ee0
Danaya <d.hristova@is-bg.net>
2026-07-24 15:24:30 +0300

    Action All added to the Game controller

--- GameZone/Controllers/GameController.cs
+++ GameZone/Controllers/GameController.cs
@@ -1,9 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GameZone.Data;
+using GameZone.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GameZone.Controllers
 {
     public class GameController : Controller
     {
-        
+        private readonly GameZoneDbContext _context;
+        public GameController(GameZoneDbContext context)
+        {
+            _context = context;
+        }   
+        public IActionResult All()
+        {
+            var games =  _context.Games
+                .Select(g => new GameViewModel
+                    {
+                        Id = g.Id,
+                        Title = g.Title,
+                        Description = g.Description,
+                        ImageUrl = g.ImageUrl,
+                        Publisher = g.PublisherName,
+                        ReleasedOn = g.ReleasedOn,
+                        GenreId = g.GenreId
+                    }).ToList();
+            return View(games);
+        }
     }
 }
---
+++ GameZone/ViewModels/GameViewModel.cs
@@ -0,0 +1,32 @@
+﻿using GameZone.Data.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GameZone.ViewModels
+{
+    public class GameViewModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        public string Description { get; set; } = string.Empty;
+
+        public string? ImageUrl { get; set; }
+
+        [Required]
+        public string Publisher { get; set; } = string.Empty;
+
+        [Required]
+        public DateTime ReleasedOn { get; set; }
+
+        [Required]
+        public int GenreId { get; set; }
+
+        [ForeignKey(nameof(GenreId))]
+        public Genre Genre { get; set; } = null!;
+    }
+}
