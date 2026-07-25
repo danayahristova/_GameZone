f14352044330bb545f2e4e38a880bbef6c6af0c2
Danaya <danaya.hristova.123yoana@gmail.com>
2026-07-25 22:46:22 +0300

    GameDatailsViewModelAdded

---
+++ GameZone/ViewModels/GameDetailsViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameZone.ViewModels
+{
+    public class GameDetailsViewModel
+    {
+
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        public string Description { get; set; } = string.Empty;
+
+        public string? ImageUrl { get; set; }
+
+        [Required]
+        public string Publisher { get; set; } = string.Empty;
+
+        [Required]
+        public DateTime ReleasedOn { get; set; }
+
+        [Required]
+        public int GenreId { get; set; }
+
+        public string Genre { get; set; } = null!;
+    }
+}
