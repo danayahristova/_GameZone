c838463561c15706dcfa6abdf05f5d40e030a125
Danaya <d.hristova@is-bg.net>
2026-07-27 11:44:38 +0300

    GameEdit view model added

---
+++ GameZone/ViewModels/GameEditViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameZone.ViewModels
+{
+    public class GameEditViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        public string Description { get; set; } = string.Empty;
+
+        public string? ImageUrl { get; set; }
+
+        [Required]
+        public string PublisherName { get; set; } = string.Empty;
+
+        [Required]
+        public DateTime ReleasedOn { get; set; }
+
+        [Required]
+        public int GenreId { get; set; }
+        public List<GenreViewModel> Genres { get; set; } = new List<GenreViewModel>();
+    }
+}
